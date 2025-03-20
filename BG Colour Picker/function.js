c06eb9fdda980e15b66629790f3f1ac994ab571f
RisingBeastVector <saiayushmanp45@gmail.com>
2025-03-21 00:58:44 +0530

    BG Colour Changer

---
+++ BG Colour Picker/function.js
@@ -0,0 +1,11 @@
+function Red() {
+  document.getElementById("page").style.backgroundColor = "red";
+}
+
+function Blue() {
+  document.getElementById("page").style.backgroundColor = "blue";
+}
+
+function Green() {
+  document.getElementById("page").style.backgroundColor = "green";
+}
