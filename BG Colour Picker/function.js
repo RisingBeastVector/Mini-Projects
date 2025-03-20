9add01bd5752790663359cc940c1db0c370f5adb
RisingBeastVector <saiayushmanp45@gmail.com>
2025-03-21 01:54:40 +0530

    Added functionality to Reset Button

--- BG Colour Picker/function.js
+++ BG Colour Picker/function.js
@@ -1,19 +1,27 @@
 function Red() {
   document.getElementById("page").style.backgroundColor = "red";
   document.getElementById("head").style.color = "white";
+  document.getElementById("button4").style.backgroundColor = "white";
+  document.getElementById("button4").style.color = "black";
 }
 
 function Blue() {
   document.getElementById("page").style.backgroundColor = "blue";
   document.getElementById("head").style.color = "white";
+  document.getElementById("button4").style.backgroundColor = "white";
+  document.getElementById("button4").style.color = "black";
 }
 
 function Green() {
   document.getElementById("page").style.backgroundColor = "green";
   document.getElementById("head").style.color = "white";
+  document.getElementById("button4").style.backgroundColor = "white";
+  document.getElementById("button4").style.color = "black";
 }
 
 function Reset() {
   document.getElementById("page").style.backgroundColor = "white";
   document.getElementById("head").style.color = "black";
+  document.getElementById("button4").style.backgroundColor = "#333333";
+  document.getElementById("button4").style.color = "white";
 }
