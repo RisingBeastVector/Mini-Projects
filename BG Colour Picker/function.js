5394f5b6205f1fe4ce656a0aa33290b1e5d898be
RisingBeastVector <saiayushmanp45@gmail.com>
2025-03-21 01:06:07 +0530

    Added Reset Button

--- BG Colour Picker/function.js
+++ BG Colour Picker/function.js
@@ -1,11 +1,19 @@
 function Red() {
   document.getElementById("page").style.backgroundColor = "red";
+  document.getElementById("head").style.color = "white";
 }
 
 function Blue() {
   document.getElementById("page").style.backgroundColor = "blue";
+  document.getElementById("head").style.color = "white";
 }
 
 function Green() {
   document.getElementById("page").style.backgroundColor = "green";
+  document.getElementById("head").style.color = "white";
+}
+
+function Reset() {
+  document.getElementById("page").style.backgroundColor = "white";
+  document.getElementById("head").style.color = "black";
 }
